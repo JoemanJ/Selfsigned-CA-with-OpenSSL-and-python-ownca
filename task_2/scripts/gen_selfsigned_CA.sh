818382bad7aa52b8f832aa1d693d8b5438c368a6
JoemanJ <diogodelazare@hotmail.com>
2025-11-11 15:46:09 -0300

    CA generation script now asks for password

--- task_2/scripts/gen_selfsigned_CA.sh
+++ task_2/scripts/gen_selfsigned_CA.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # This script generates a new private key and certificate sign request for the CA
 # The CSR is signed by the CA's own private key, that is, the CA is selfsigned
-#
+# 
+# This script will prompt you for a password for the newly generated CA
+# The password must be between 4 and 1024 characters long
+# 
 # The CA is created at "../certs/CA.crt"
 # The CA private key is created at "../keys/CA.key"
 
@@ -14,7 +17,6 @@ openssl req -newkey rsa:4096 \
             -x509 \
             -sha256 \
             -days 3650 \
-            -nodes \
             -out ../certs/CA.crt \
             -keyout ../keys/CA.key \
             -subj "/C=BR/ST=Espirito Santo/L=Vitoria/O=UFES - Universidade Federal do Espirito Santo/OU=CT/CN=SegurancaEmComputacao.com"
