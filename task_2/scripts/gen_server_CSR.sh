b0a690366473fbe4010277da25770e6ede170dce
JoemanJ <diogodelazare@hotmail.com>
2025-11-10 21:24:03 -0300

    added server CSR generation script

---
+++ task_2/scripts/gen_server_CSR.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# This script generates a new private key and certificate sign request for the 
+# server
+# 
+# The generated CSR is not selfsigned and must be signed by the CA afterwards
+# by using the sign_server_CSR.sh script
+#
+# The server request is created at "../reqs/server.req"
+# The server private key is created at "../keys/server.key"
+
+cd "$(dirname "$0")"
+
+mkdir ../keys
+mkdir ../reqs
+
+# Generate RSA key
+openssl genrsa -out ../keys/server.key 4096
+
+# Generate CSR
+openssl req -new \
+            -key ../keys/server.key \
+            -out ../reqs/server.csr \
+            -subj "/C=BR/ST=Espirito Santo/L=Vitoria/O=UFES - Universidade Federal do Espirito Santo/OU=CT/CN=localhost"
